9edccfab7d7030191d4df356bff471f8600d5553
ClaudiaTeh <s10221023@connect.np.edu.sg>
2024-01-05 23:24:40 +0800

    Deleted unnecessary staging files and used only one to calculate profit

--- models/staging/stg_nwtdata_order_detail.sql
+++
@@ -1,2 +0,0 @@
-select *,
-from {{ ref ('nwtdata_order_details')}}
--- models/staging/stg_nwtdata_orders.sql
+++ models/staging/stg_nwtdata_orders.sql
@@ -1,2 +1,8 @@
-select *,
-from {{ ref ('nwtdata_orders')}}
+SELECT od.*,
+    (od.unitprice * (1 - od.discount) - p.unitprice) AS orderprofit
+FROM
+    {{ ref('nwtdata_order_detail')}} AS od
+JOIN
+    {{ ref('nwtdata_product')}} AS p
+ON
+    od.productid = p.productid
