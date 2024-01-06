30d293dab1fe05c71bbdfc083bf7e343ca849d95
Ern Qi <huohernqi@gmail.com>
2024-01-06 18:03:53 +0800

    fixed customer name error in top customer staging

--- models/Reporting/stg_nwtdata_TopCustomers.sql
+++ models/Reporting/stg_nwtdata_TopCustomers.sql
@@ -2,11 +2,12 @@
 
 SELECT
   c.CustomerId,
-  c.CustomerName,
   SUM((p.unitprice * (1 - discount)) * Quantity) AS CustomerSales
 FROM {{ ref('nwtdata_customers') }} AS c
 JOIN {{ ref('nwtdata_orders') }} AS o ON c.CustomerId = o.CustomerId
 JOIN {{ ref('nwtdata_order_detail') }} AS od ON od.OrderId = o.OrderID
 JOIN {{ ref('nwtdata_product') }} AS p ON p.ProductId = od.ProductId
 
-GROUP BY c.CustomerId, c.CustomerName
+GROUP BY c.CustomerId
+ORDER BY
+  CustomerSales DESC
