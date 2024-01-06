5ff1d2f709f15ca6e0bc3a41db08b34be4339bfa
s10242248 <153148149+s10242248@users.noreply.github.com>
2024-01-06 17:10:02 +0800

    Added category id into the sales by category view

--- models/Reporting/stg_nwtdata_salesbycategory.sql
+++ models/Reporting/stg_nwtdata_salesbycategory.sql
@@ -1,11 +1,12 @@
 {{ config(materialized='view') }}
 
 SELECT
-  c.CategoryName,
+  c.CategoryId,
+  CategoryName,
   SUM((p.unitprice * (1 - discount)) * Quantity) AS ProductSales
 
 FROM {{ ref('nwtdata_product') }} AS p
 JOIN {{ ref('nwtdata_order_detail') }} AS o ON p.ProductId = o.ProductId
 JOIN {{ ref('nwtdata_category') }} AS c ON p.CategoryId = c.CategoryId
 
-GROUP BY c.CategoryName
+GROUP BY CategoryName, c.CategoryId
