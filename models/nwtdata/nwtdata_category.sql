49d175c786fd35ee5cc5ca5928519db6ec5e31a1
S10243306 <s10243306@connect.np.edu.sg>
2024-01-06 14:44:31 +0800

    data cleaning

--- models/nwtdata/nwtdata_category.sql
+++ models/nwtdata/nwtdata_category.sql
@@ -4,6 +4,13 @@ Select *
 From 
 NWTDATA.NWT.CATEGORY;
 
+SELECT *
+FROM NWTDATA.NWT.CATEGORY
+WHERE
+  categoryID LIKE '%NULL%' OR
+  categoryName LIKE '%NULL%' OR
+  description LIKE '%NULL%' OR
+  picture LIKE '%NULL%';
 
 
 
--- models/nwtdata/nwtdata_order_detail.sql
+++ models/nwtdata/nwtdata_order_detail.sql
@@ -8,3 +8,8 @@ NWTDATA.NWT.ORDER_DETAIL;
 -- FROM NWTDATA.NWT.ORDER_DETAIL
 -- GROUP BY orderid
 -- HAVING count > 1;
+
+-- checking for any negative values in unitprice, quantity and discount
+SELECT *
+FROM NWTDATA.NWT.ORDER_DETAIL
+WHERE unitPrice < 0 OR quantity < 0 OR discount < 0;
