b9da54f7881efbdcdd8c959de855d46a7dab94d3
s10242248 <153148149+s10242248@users.noreply.github.com>
2024-01-06 21:04:31 +0800

    created regional sales view and standerdised naming conventions

---
+++ models/Reporting/stg_nwtdata_regionalsales.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='view') }}
+
+SELECT
+  Region,
+  SUM((p.unitprice * (1 - discount)) * Quantity) AS RegionSales
+
+FROM {{ ref('nwtdata_region') }} AS r
+JOIN {{ ref('nwtdata_territory') }} AS t ON r.RegionId = t.RegionId
+JOIN {{ ref('nwtdata_employee_territory') }} AS et ON t.TerritoryId = et.TerritoryId
+JOIN {{ ref('nwtdata_employee') }} AS e ON et.EmployeeId = e.EmployeeId
+JOIN {{ ref('nwtdata_orders') }} AS o ON e.EmployeeId = o.EmployeeId
+JOIN {{ ref('nwtdata_order_detail') }} AS od ON o.OrderId = od.OrderId
+JOIN {{ ref('nwtdata_product') }} AS p ON od.ProductId = p.ProductId
+
+GROUP BY Region
