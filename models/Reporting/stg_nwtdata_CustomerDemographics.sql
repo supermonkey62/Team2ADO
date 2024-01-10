d510193e37fc2ed50d8278b881fcf0885c57e325
Ern Qi <huohernqi@gmail.com>
2024-01-10 15:16:25 +0800

    Fixed customer demographics duplicate rows error

--- models/Reporting/stg_nwtdata_CustomerDemographics.sql
+++ models/Reporting/stg_nwtdata_CustomerDemographics.sql
@@ -1,15 +1,32 @@
+-- {{ config(materialized='view') }}
+
+-- SELECT DISTINCT
+--     C.ContactName AS CustomerContactName,
+--     C.CompanyName AS CustomerCompanyName,
+--     C.ContactTitle AS CustomerContactTitle,
+--     C.City AS CustomerCity,
+--     C.Region AS CustomerRegion,
+--     T.TerritoryDescription AS CustomerTerritory
+-- FROM {{ ref('nwtdata_customers') }} AS C
+-- JOIN {{ ref('nwtdata_orders') }} AS O ON C.CustomerID = O.CustomerID
+-- JOIN {{ ref('nwtdata_employee') }} AS E ON O.EmployeeID = E.EmployeeID
+-- JOIN {{ ref('nwtdata_employee_territory') }} AS ET ON E.EmployeeID = ET.EmployeeID
+-- JOIN {{ ref('nwtdata_territory') }} AS T ON ET.TerritoryID = T.TerritoryID
+
 {{ config(materialized='view') }}
 
 SELECT
-    C.ContactName AS CustomerContactName,
-    C.CompanyName AS CustomerCompanyName,
-    C.ContactTitle AS CustomerContactTitle,
-    C.City AS CustomerCity,
-    C.Region AS CustomerRegion,
-    T.TerritoryDescription AS CustomerTerritory
+    C.CustomerID,
+    MAX(C.ContactName) AS CustomerContactName,
+    MAX(C.CompanyName) AS CustomerCompanyName,
+    MAX(C.ContactTitle) AS CustomerContactTitle,
+    MAX(C.City) AS CustomerCity,
+    MAX(C.Region) AS CustomerRegion,
+    MAX(T.TerritoryDescription) AS CustomerTerritory
 FROM {{ ref('nwtdata_customers') }} AS C
 JOIN {{ ref('nwtdata_orders') }} AS O ON C.CustomerID = O.CustomerID
 JOIN {{ ref('nwtdata_employee') }} AS E ON O.EmployeeID = E.EmployeeID
 JOIN {{ ref('nwtdata_employee_territory') }} AS ET ON E.EmployeeID = ET.EmployeeID
 JOIN {{ ref('nwtdata_territory') }} AS T ON ET.TerritoryID = T.TerritoryID
+GROUP BY C.CustomerID
 
