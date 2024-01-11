452458a7fabe32758647791be2dbe2cc156d145e
S10243306 <s10243306@connect.np.edu.sg>
2024-01-11 12:13:58 +0800

    deleted extra category test file and cleaned another column in customers

--- models/cleaning/customers_cleaned.sql
+++ models/cleaning/customers_cleaned.sql
@@ -11,3 +11,77 @@ SET region = CASE
                ELSE region
              END
 WHERE region LIKE '%NULL%';
+
+UPDATE NWTDATA.NWT.CUSTOMERS
+SET postalCode = CASE 
+            WHEN postalCode LIKE '%NULL%' THEN 'NONE'
+            ELSE postalCode
+          END
+WHERE postalCode LIKE '%NULL%';
+
+
+SELECT
+  'customerID' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE customerID LIKE '%NULL%'
+UNION ALL
+SELECT
+  'companyName' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE companyName LIKE '%NULL%'
+UNION ALL
+SELECT
+  'contactName' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE contactName LIKE '%NULL%'
+UNION ALL
+SELECT
+  'contactTitle' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE contactTitle LIKE '%NULL%'
+UNION ALL
+SELECT
+  'address' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE address LIKE '%NULL%'
+UNION ALL
+SELECT
+  'city' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE city LIKE '%NULL%'
+UNION ALL
+SELECT
+  'region' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE region LIKE '%NULL%'
+UNION ALL
+SELECT
+  'postalCode' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE postalCode LIKE '%NULL%'
+UNION ALL
+SELECT
+  'country' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE country LIKE '%NULL%'
+UNION ALL
+SELECT
+  'phone' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE phone LIKE '%NULL%'
+UNION ALL
+SELECT
+  'fax' AS column_name,
+  COUNT(*) AS null_count
+FROM NWTDATA.NWT.CUSTOMERS
+WHERE fax LIKE '%NULL%';
