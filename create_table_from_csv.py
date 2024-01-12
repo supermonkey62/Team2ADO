2e576ac797620fd4b8f4af252a849749c5b61f2d
supermonkey62 <kennethbwwu@gmail.com>
2024-01-13 00:13:19 +0800

    26th try

--- create_table_from_csv.py
+++ create_table_from_csv.py
@@ -61,11 +61,11 @@ for file in files:
     print(columns)
     
     # Create the table using TEMPLATE with column definitions
-    # cs.execute(f"CREATE TABLE IF NOT EXISTS {table_name} USING TEMPLATE ({','.join([f'{col[0]} {col[1]}' for col in columns])})")
+    cs.execute(f"CREATE TABLE IF NOT EXISTS {table_name} USING TEMPLATE ({','.join([f'{col[0]} {col[1]}' for col in columns])})")
 
 
     # Load data into the table
-    # cs.execute(f"COPY INTO {table_name} FROM @NWT_STAGING/{file} FILE_FORMAT = '{file_format_name}'")
+    cs.execute(f"COPY INTO {table_name} FROM @NWT_STAGING/{file} FILE_FORMAT = '{file_format_name}'")
 
 cs.close()
 ctx.close()
