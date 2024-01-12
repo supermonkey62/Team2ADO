4dae46b74f58bfffaf99bfb1704536cd55984cec
supermonkey62 <130966363+supermonkey62@users.noreply.github.com>
2024-01-12 15:18:14 +0800

    Update upload_to_s3.py

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -2,6 +2,17 @@ import boto3
 import os
 import requests
 
+def download_and_upload_to_s3(download_url, object_key, s3_bucket, aws_access_key_id, aws_secret_access_key, region_name):
+    # Download data from GitHub
+    response = requests.get(download_url)
+    file_content = response.content
+
+    # Upload data to Amazon S3
+    s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
+    s3.put_object(Body=file_content, Bucket=s3_bucket, Key=object_key)
+
+    print(f'Data loaded into S3 bucket: {s3_bucket}, Object key: {object_key}')
+
 # AWS credentials
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
@@ -12,14 +23,10 @@ github_raw_url = 'https://raw.githubusercontent.com/just4jc/Northwind-Traders-Da
 
 # Amazon S3 bucket and object key
 s3_bucket = 'team2adonwtbucket'
-s3_object_key = 'category.csv'
 
-# Download data from GitHub
-response = requests.get(github_raw_url)
-file_content = response.content
+# Extract the object key from the download URL
+object_key = github_raw_url.split('/')[-1]
 
-# Upload data to Amazon S3
-s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region_name)
-s3.put_object(Body=file_content, Bucket=s3_bucket, Key=s3_object_key)
+# Now you have the object key and download URL, and you can proceed with downloading and uploading
+download_and_upload_to_s3(github_raw_url, object_key, s3_bucket, aws_access_key_id, aws_secret_access_key, region_name)
 
-print(f'Data loaded into S3 bucket: {s3_bucket}, Object key: {s3_object_key}')
