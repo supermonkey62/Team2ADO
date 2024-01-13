4e97b7162d663493d347967289583e1b56f390eb
S10243306 <s10243306@connect.np.edu.sg>
2024-01-13 19:10:28 +0800

    changed naming to raw files

--- models/staging/stg_nwtdata_topcustomers.sql
+++ models/staging/stg_nwtdata_topcustomers.sql
@@ -3,10 +3,10 @@
 SELECT
   c.CustomerId,
   SUM((od.unitprice * (1 - discount)) * Quantity) AS CustomerSales
-FROM {{ ref('nwtdata_customers') }} AS c
-JOIN {{ ref('nwtdata_orders') }} AS o ON c.CustomerId = o.CustomerId
-JOIN {{ ref('nwtdata_order_detail') }} AS od ON od.OrderId = o.OrderID
-JOIN {{ ref('nwtdata_product') }} AS p ON p.ProductId = od.ProductId
+FROM {{ ref('raw_customers') }} AS c
+JOIN {{ ref('raw_orders') }} AS o ON c.CustomerId = o.CustomerId
+JOIN {{ ref('raw_order_detail') }} AS od ON od.OrderId = o.OrderID
+JOIN {{ ref('raw_product') }} AS p ON p.ProductId = od.ProductId
 
 GROUP BY c.CustomerId
 ORDER BY
