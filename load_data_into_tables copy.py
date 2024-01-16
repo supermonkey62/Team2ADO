14cb55cba80be8c387b64c3e28bc59e21cbec26b
supermonkey62 <kennethbwwu@gmail.com>
2024-01-16 21:09:00 +0800

    load data

--- load_data_into_tables copy.py
+++ load_data_into_tables copy.py
@@ -1,5 +1,7 @@
 import os
 import snowflake.connector
+import csv
+from io import StringIO
 
 # Replace with your Snowflake credentials
 account = os.getenv('DBT_ACCOUNT')
@@ -10,7 +12,6 @@ schema = os.getenv('SNOWFLAKE_SCHEMA')
 database = 'NWTDATA'
 stage_name = 'NWT_STAGING'
 load_format_name = 'load_csv_format'
-
 # Connect to Snowflake
 ctx = snowflake.connector.connect(
     account=account,
@@ -40,10 +41,33 @@ for file in files:
     # Check if the table already exists
     check_table_query = f"SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'RAW_{table_name.upper()}' AND TABLE_SCHEMA = 'NWT';"
     cs.execute(check_table_query)
-    table_count = cs.fetchone()[0]
+    table_exists = cs.fetchone()[0] > 0
 
-    if table_count > 0:
-        # Table is not empty, load data into the table
+    if  table_exists:
+        # # Use INFER_SCHEMA to get column definitions
+        # infer_schema_query = f"SELECT * FROM TABLE(INFER_SCHEMA(LOCATION=>'@NWT_STAGING/{file_name}', FILE_FORMAT=>'{file_format_name}'))"
+        # print(infer_schema_query)
+        # cs.execute(infer_schema_query)
+        # columns = cs.fetchall()
+
+        # # Access the column names
+        # column_names = [col[0] for col in columns]
+        # print("Column Names:", column_names)
+
+        # # Construct the column definitions
+        # column_definitions = [f'"{col[0].replace(" ", "")}" {col[1]}' for col in columns]
+
+        # # Join the column definitions into a string
+        # columns_string = ',\n\t'.join(column_definitions)
+
+        # print(columns_string)
+
+        # # Create the table using specified column definitions
+        # create_table_query = f"CREATE TABLE IF NOT EXISTS NWTDATA.NWT.RAW_{table_name} ({columns_string});"
+        # print(create_table_query)
+        # cs.execute(create_table_query)
+
+        # print(f"Successfully created RAW_{table_name}")
 
         # Load data into the table
         load_data_query = f"COPY INTO NWTDATA.NWT.RAW_{table_name} FROM @NWT_STAGING/{file_name} FILE_FORMAT = '{load_format_name}';"
@@ -51,11 +75,13 @@ for file in files:
         cs.execute(load_data_query)
 
         print(f"Successfully copied {file_name} into RAW_{table_name}")
+
     else:
-        print(f"Table {table_name} is empty.")
+        print(f"Table {table_name} does not exist.")
 
 cs.close()
 ctx.close()
+
 
 
 
