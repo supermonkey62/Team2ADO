b42ffe2563e237447e83e0a67da32c7055f26a98
Ern Qi <huohernqi@gmail.com>
2024-01-17 02:29:42 +0800

    byebye

--- migrations/V1.1.1__initial_objects.sql
+++ migrations/V1.1.1__initial_objects.sql
@@ -2,10 +2,12 @@ USE SCHEMA NWT;
 ALTER TABLE RAW_PRODUCT ADD COLUMN Unitcost FLOAT;
 
 
-UPDATE RAW_PRODUCT 
-SET RAW_PRODUCT.UnitCost = pf.UnitCost
-FROM RAW_PRODUCT AS p
-INNER JOIN RAW_PRODUCT_FRESH AS pf
-ON p.productID = pf.productID;
+-- UPDATE RAW_PRODUCT 
+-- SET RAW_PRODUCT.UnitCost = pf.UnitCost
+-- FROM RAW_PRODUCT AS p
+-- INNER JOIN RAW_PRODUCT_FRESH AS pf
+-- ON p.productID = pf.productID;
 
-
+-- SELECT p.productID, p.UnitCost AS OldUnitCost, pf.UnitCost AS NewUnitCost
+-- FROM RAW_PRODUCT AS p
+-- INNER JOIN RAW_PRODUCT_FRESH AS pf ON p.productID = pf.productID;
