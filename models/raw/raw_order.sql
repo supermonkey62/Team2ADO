eeeb1bb738f89b5887f90b41dbac19ddb36a3e15
supermonkey62 <kennethbwwu@gmail.com>
2024-01-16 21:28:33 +0800

    append new orders

--- models/raw/raw_order.sql
+++ models/raw/raw_order.sql
@@ -1,5 +1,10 @@
-{{ config (materialized='table')}}
+ {{ config(materialized='incremental', unique_key='ORDERID') }}
 
-select *
-from {{ source('NWT', 'RAW_ORDER_UPDATED') }}
+SELECT *
+FROM {{ ref ('raw_order_fresh') }}
+ {% if is_incremental() %}
+WHERE CAST(ORDERID AS BIGINT) > (SELECT MAX(CAST(ORDERID AS BIGINT))  FROM {{this}})
+{% endif %}
+
+
 
