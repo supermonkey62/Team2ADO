2297e87f901baa7cd6538ad2a343dde3e062fa17
Ern Qi <huohernqi@gmail.com>
2024-01-18 01:15:45 +0800

    updated product reporting to include profit

--- models/staging/stg_nwtdata_productsales.sql
+++ models/staging/stg_nwtdata_productsales.sql
@@ -11,7 +11,10 @@ SELECT
   QuantityPerUnit,
   reorderlevel,
   discontinued,
-  (od.unitprice * (1 - discount)) * Quantity AS Sales
+  unitcost,
+  (od.unitprice * (1 - discount)) * Quantity AS Sales,
+  (od.unitprice * (1 - discount) * Quantity) - (unitcost*quantity) AS Profit,
+  p.unitprice - unitcost AS ProfitMargin
 
 FROM {{ ref('raw_product') }} AS p
 JOIN {{ ref('raw_order_detail') }} AS od ON p.ProductId = od.ProductId
