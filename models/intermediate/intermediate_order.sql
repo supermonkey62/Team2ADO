a3d6e64f86f9f7739593a695b2c8ff6931958426
ClaudiaTeh <s10221023@connect.np.edu.sg>
2024-01-26 23:30:47 +0800

    corrected intermediate files. left with duplicated error in order and order detail

--- models/intermediate/intermediate_order.sql
+++ models/intermediate/intermediate_order.sql
@@ -15,25 +15,23 @@ SELECT
   CASE WHEN shipRegion IS NULL THEN 'None' ELSE shipRegion END AS shipRegion,
   CASE WHEN shipPostalCode IS NULL THEN 'None' ELSE shipPostalCode END AS shipPostalCode,
   CASE WHEN shipCountry IS NULL THEN 'None' ELSE shipCountry END AS shipCountry
-
 FROM {{ source('NWT', 'RAW_ORDER_FRESH') }}
 
 UNION ALL
 
 SELECT
-  --CASE WHEN orderID = 'NULL' THEN 0 ELSE orderID END AS orderID,
+  CASE WHEN orderID IS NULL THEN 0 ELSE orderID END AS orderID,
   CASE WHEN customerID = 'NULL' THEN 'None' ELSE customerID END AS customerID,
- -- CASE WHEN employeeID = 'NULL' THEN 0 ELSE employeeID END AS employeeID,
-  CASE WHEN orderDate = 'NULL' THEN TO_DATE('09-09-9999', 'MM-DD-YYYY') ELSE TO_DATE(orderDate, 'MM-DD-YYYY') END AS orderDate,
-  CASE WHEN requiredDate = 'NULL' THEN TO_DATE('09-09-9999', 'MM-DD-YYYY') ELSE TO_DATE(requiredDate, 'MM-DD-YYYY') END AS requiredDate,
-  CASE WHEN shippedDate = 'NULL' THEN TO_DATE('09-09-9999', 'MM-DD-YYYY') ELSE TO_DATE(shippedDate, 'MM-DD-YYYY') END AS shippedDate,
-  --CASE WHEN shipVia = 'NULL' THEN 0 ELSE shipVia END AS shipVia,
-  --CASE WHEN freight = 'NULL' THEN 0 ELSE freight END AS freight,
+  CASE WHEN employeeID IS NULL THEN 0 ELSE employeeID END AS employeeID,
+  CASE WHEN orderDate IS NULL THEN TO_DATE('09-09-9999', 'MM-DD-YYYY') ELSE orderDate END AS orderDate,
+  CASE WHEN requiredDate IS NULL THEN TO_DATE('09-09-9999', 'MM-DD-YYYY') ELSE requiredDate END AS requiredDate,
+  CASE WHEN shippedDate IS NULL THEN TO_DATE('09-09-9999', 'MM-DD-YYYY') ELSE shippedDate END AS shippedDate,
+  CASE WHEN shipVia IS NULL THEN 0 ELSE shipVia END AS shipVia,
+  CASE WHEN freight IS NULL THEN 0 ELSE freight END AS freight,
   CASE WHEN shipName = 'NULL' THEN 'None' ELSE shipName END AS shipName,
   CASE WHEN shipAddress = 'NULL' THEN 'None' ELSE shipAddress END AS shipAddress,
   CASE WHEN shipCity = 'NULL' THEN 'None' ELSE shipCity END AS shipCity,
   CASE WHEN shipRegion = 'NULL' THEN 'None' ELSE shipRegion END AS shipRegion,
   CASE WHEN shipPostalCode = 'NULL' THEN 'None' ELSE shipPostalCode END AS shipPostalCode,
   CASE WHEN shipCountry = 'NULL' THEN 'None' ELSE shipCountry END AS shipCountry
-
 FROM {{ source('NWT', 'RAW_ORDER_FRESH') }}
--- models/intermediate/intermediate_shipper.sql
+++ models/intermediate/intermediate_shipper.sql
@@ -10,7 +10,7 @@ FROM {{ source('NWT', 'RAW_SHIPPER') }}
 UNION ALL
 
 SELECT 
---  CASE WHEN shipperID = 'NULL' THEN 0 ELSE shipperID END AS shipperID,
+  CASE WHEN shipperID IS NULL THEN 0 ELSE shipperID END AS shipperID,
   CASE WHEN companyName = 'NULL' THEN 'None' ELSE companyName END AS companyName,
   CASE WHEN phone = 'NULL' THEN 'None' ELSE phone END AS phone
 
