283d468b60e36d5790b5a3435c254859893d85ca
S10243306 <s10243306@connect.np.edu.sg>
2024-01-26 22:07:18 +0800

    territory changes

--- models/intermediate/intermediate_territory.sql
+++ models/intermediate/intermediate_territory.sql
@@ -3,5 +3,5 @@
 SELECT 
   CASE WHEN territoryID IS NULL THEN 'None' ELSE territoryID END AS territoryID,
   CASE WHEN territoryDescription IS NULL THEN 'None' ELSE territoryDescription END AS territoryDescription,
-  CASE WHEN regionID IS NULL THEN 'None' ELSE regionID END AS regionID
+  CASE WHEN regionID IS NULL THEN 0 ELSE regionID END AS regionID
 FROM {{ source('NWT', 'RAW_TERRITORY') }}
